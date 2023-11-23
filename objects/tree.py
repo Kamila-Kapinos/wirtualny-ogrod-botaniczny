37872ad4c7903f85ede275a061f804af03223e3d
GalaxUniv <afterhoursbilly@gmail.com>
2023-11-23 14:47:58 +0100

    feat: add new methods to garden plant plot tree

--- objects/tree.py
+++ objects/tree.py
@@ -1,6 +1,21 @@
 from .plant import Plant
+from .fruit import Fruit
 
 
 class Tree(Plant):
-    def __init__(self, name: str, emoji: str = '🌲'):
+    def __init__(self, name: str, emoji: str = '🌳'):
         super().__init__(name, emoji)
+        self.blooming = False  # tree can bloom and later bears fruit.
+        self.bears_fruit = False
+        self.leaves = False
+
+    def foliate(self):
+        pass
+
+    def drop_leaves(self):
+        pass
+
+    def bear_fruit(self):
+        # Create fruit class
+        self.bears_fruit = Fruit()
+        pass
