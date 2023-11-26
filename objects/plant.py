ab45ca5152d8609360f9decd84336ca0ed9e7c22
GalaxUniv <afterhoursbilly@gmail.com>
2023-11-26 03:00:57 +0100

    fix: fix tab spacing

--- objects/plant.py
+++ objects/plant.py
@@ -10,35 +10,35 @@ class Plant:
         self.is_watered = False
         self.is_in_sunlight = False
 
-        def grow(self):
-            if list(self.care_requirements.values()) == [1, 1]:
-                self.plant_level += 1
+    def grow(self):
+        if list(self.care_requirements.values()) == [1, 1]:
+            self.plant_level += 1
 
-        def wither(self):
-            if list(self.care_requirements.values()) == [-1, 1]:
-                pass  # remove plant form plot
+    def wither(self):
+        if list(self.care_requirements.values()) == [-1, 1]:
+            pass  # remove plant form plot
 
-        def day_summary(self):
-            # TODO - add a day summary to the garden class
-            if self.is_watered == True:
-                self.care_requirements["water_frequency"] += 1
-            else:
-                pass
-            if self.is_in_sunlight == True:
-                self.care_requirements["sunlight_requirements"] += 1
-            else:
-                pass
+    def day_summary(self):
+        # TODO - add a day summary to the garden class
+        if self.is_watered == True:
+            self.care_requirements["water_frequency"] += 1
+        else:
+            pass
+        if self.is_in_sunlight == True:
+            self.care_requirements["sunlight_requirements"] += 1
+        else:
+            pass
 
-        def reproduction(self):
-            # at this level plant is able to create new plants in adjacent plots
-            if self.plant_level == 5:
-                pass
+    def reproduction(self):
+        # at this level plant is able to create new plants in adjacent plots
+        if self.plant_level == 5:
+            pass
 
-        def sunlight(self):
-            self.is_in_sunlight = True
+    def sunlight(self):
+        self.is_in_sunlight = True
 
-        def water(self):
-            self.is_watered = True
+    def water(self):
+        self.is_watered = True
 
-        def status(self):
-            return f"{self.name} is a {self.__class__.__name__} at level {self.plant_level}."
+    def status(self):
+        return f"{self.name} is a {self.__class__.__name__} at level {self.plant_level}."
