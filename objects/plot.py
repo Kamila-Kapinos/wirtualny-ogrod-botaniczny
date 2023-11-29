0208a0820146b8a7c1da258d6ec7ffe74ca335cf
GalaxUniv <afterhoursbilly@gmail.com>
2023-11-29 10:44:50 +0100

    feat: add water, harves methods

--- objects/plot.py
+++ objects/plot.py
@@ -11,4 +11,18 @@ class Plot:
         self.contained_plant = plant
 
     def remove_plant(self):
+        if self.contained_plant is None:
+            raise ValueError("Plot is empty")
+        
+        # method could return some information about the plant that was removed
         self.contained_plant = None
+
+    def water(self):
+        if self.contained_plant is None:
+            raise ValueError("Plot is empty")
+        self.contained_plant.water()
+
+    def harvest(self):
+        if self.contained_plant is None:
+            raise ValueError("Plot is empty")
+        self.contained_plant.harvest()
