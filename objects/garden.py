cfb9bd70f393b351680c5d9156338f86ab5f24a5
GalaxUniv <afterhoursbilly@gmail.com>
2023-11-29 10:44:22 +0100

    feat: add water, harvest, day_summary and show methods

--- objects/garden.py
+++ objects/garden.py
@@ -1,5 +1,7 @@
 from .plant import Plant
 from .plot import Plot
+from random import randint
+import sys
 
 
 class Garden:
@@ -15,29 +17,61 @@ class Garden:
         self.plots[x][y].remove_plant()
         # maybe add a warning if user wants to remove a plant, so he doesn't do it by mistake
 
-    def about(self):
-        # loop over plots and if value is not 0 print the plot with its position and plant.
-        pass
-
-    def water(self, x: int, y: int):
+    def water(self, x: int = None, y: int = None):
         # water all plots by default or water a specific plot given x,y coordinates
-        pass
+        if x or y is None:
+            for row in self.plots:
+                for plot in row:
+                    plot.water()
+        else:
+            self.plots[x][y].water()
 
-    def harvest(self, x: int, y: int):
+    def harvest(self, x: int = None, y: int = None):
         # harvest all plots by default or harvest a specific plot given x,y coordinates
-        pass
+        if x or y is None:
+            for row in self.plots:
+                for plot in row:
+                    plot.harvest()
+        else:
+            self.plots[x][y].harvest()
 
     def end_day(self):
         # ends day and calculates plant growth
-
         self._sunny_day()
         self._rainy_day()
-        pass
+
+        # Iterate over all plots in the garden
+        for row in self.plots:
+            for plot in row:
+                if plot.contained_plant:
+                    # Update each plant
+                    plot.contained_plant.update(self.sunny, self.raining)
+        # END OF ITERATION
 
     def _sunny_day(self):
         # randomly choose if a day is sunny or not
-        pass
+        if randint(0, 7) >= 3:
+            self.sunny = True
+        else:
+            self.sunny = False
 
     def _rainy_day(self):
-        # randomly choose if a day is rainy or not
-        pass
+        if randint(0, 7) >= 5:
+            self.raining = True
+        else:
+            self.raining = False
+
+    def show(self):
+        n = len(self.plots[0])  # Assuming all rows have equal number of plots
+        horizontal_separator = '+' + ('---+' * n)
+        sys.stdout.write(horizontal_separator + '\n')
+        for row in self.plots:
+            sys.stdout.write('|')  # Start of row
+            for plot in row:
+                if plot.contained_plant:
+                    sys.stdout.write(f" {plot.contained_plant.emoji} |")  # Plot with plant
+                else:
+                    sys.stdout.write(" 🐥 |")  # Empty plot
+            sys.stdout.write('\n')
+            sys.stdout.write(horizontal_separator + '\n')
+        sys.stdout.flush()
