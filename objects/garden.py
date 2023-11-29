1f0dc745a43949b2527499755f15817999745490
GalaxUniv <afterhoursbilly@gmail.com>
2023-11-29 14:48:06 +0100

    add reproduce method WIP

--- objects/garden.py
+++ objects/garden.py
@@ -6,9 +6,24 @@ import sys
 
 class Garden:
     def __init__(self, n: int, m: int):
+        self.n, self.m = n, m
         self.plots = [[Plot() for _ in range(n)] for _ in range(m)]
         self.sunny = False
         self.raining = False
+        self.day_counter = 0
+
+    def reproduce(self, x: int, y: int):
+        if self.plots[x][y].check_reproduce_plant():  # roślina jest gotowa do reprodukcji
+            available_plots = []
+            # FUdaj dodaać funkcję która sprawdza czy są wolne miejsca obok
+            if not self.plots[x + 1][y].contained_plant:
+                available_plots.append((x + 1, y))
+            pass
+            # GET RANGE OF AVALIBLE PLOTS and get one random plot
+            x, y = available_plots[randint(0, len(available_plots) - 1)]
+            self.plots[x][y] = self.plots[x][y].add_plant(self.plots[x][y].contained_plant.get_dna())
+        else:
+            pass
 
     def add_plant(self, plant: Plant, x: int = 0, y: int = 0):
         self.plots[x][y].place_plant(plant)
@@ -36,10 +51,6 @@ class Garden:
             self.plots[x][y].harvest()
 
     def end_day(self):
-        # ends day and calculates plant growth
-        self._sunny_day()
-        self._rainy_day()
-
         # Iterate over all plots in the garden
         for row in self.plots:
             for plot in row:
@@ -47,6 +58,11 @@ class Garden:
                     # Update each plant
                     plot.contained_plant.update(self.sunny, self.raining)
         # END OF ITERATION
+
+        # ends day and calculates plant growth
+        self._sunny_day()
+        self._rainy_day()
+        self.day_counter += 1
 
     def _sunny_day(self):
         # randomly choose if a day is sunny or not
@@ -71,7 +87,7 @@ class Garden:
                 if plot.contained_plant:
                     sys.stdout.write(f" {plot.contained_plant.emoji} |")  # Plot with plant
                 else:
-                    sys.stdout.write(" 🐥 |")  # Empty plot
+                    sys.stdout.write(" 🟤 |")  # Empty plot
             sys.stdout.write('\n')
             sys.stdout.write(horizontal_separator + '\n')
         sys.stdout.flush()
