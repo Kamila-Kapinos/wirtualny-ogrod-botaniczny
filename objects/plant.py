ec5af6589bdad85715b29bede738a7230d5e3910
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-06 16:19:12 +0100

    feat: private variables and new methods names small reformating

--- objects/plant.py
+++ objects/plant.py
@@ -3,76 +3,76 @@ import copy
 
 class Plant:
     def __init__(self, name: str, emoji: str = '🌱',
-                 water_frequency: int = 3, sunlight_exposure: int = 5):
+                 water_frequency: int = 3, sunlight_exposure: int = 5,
+                 reproduce_level: int = 5):
         self.ready_to_reproduce = False
-        self.name = name
+        self._name = name
         self.emoji = str(emoji)  # in case emoji is not passed as a string
-        self.plant_level = 1
+        self._plant_level = 1
         self.is_dead = False
-        self.reproduce_cooldown = 5
-        self.care_requirements = {
+        self._reproduce_level = reproduce_level
+        self._reproduce_cooldown = 5
+        self._care_requirements = {
             "water_frequency": water_frequency,
             "sunlight_exposure": sunlight_exposure,
         }
-        self.care_record = {
+        self._care_record = {
             "water_frequency": water_frequency,
             "sunlight_exposure": sunlight_exposure,
         }
 
-    def requirements_checker(self):
-        for key, value in self.care_requirements.items():
-            survive = range(- (self.plant_level - 1), self.plant_level)  # if in this range don't grow but don't die
-            if self.care_record[key] - self.care_requirements[key] in (-1, 0, 1):
+    def _requirements_checker(self):
+        for key, value in self._care_requirements.items():
+            survive = range(- (self._plant_level - 1), self._plant_level)  # if in this range don't grow but don't die
+            if self._care_record[key] - self._care_requirements[key] in (-1, 0, 1):
                 pass
-            elif self.care_record[key] - self.care_requirements[key] in survive:
+            elif self._care_record[key] - self._care_requirements[key] in survive:
                 print("Warning! Take care of your plant")
                 return 0
             else:
                 return -1
         return 1
 
-    def grow(self):
-        self.plant_level += self.requirements_checker()
+    def _grow(self):
+        self._plant_level += self._requirements_checker()
 
-    def wither(self):
-        if self.plant_level < 0:
+    def _wither(self):
+        if self._plant_level < 0:
             self.is_dead = True
 
-    def reproduction(self):
+    # TODO: maybe rename this method and use it plot.py
+    def reproduction(self) -> bool:
         # at this level plant is able to create new plants in adjacent plots
-        self.ready_to_reproduce = self.plant_level >= 5
+        self.ready_to_reproduce = self._plant_level >= self._reproduce_level
         return self.ready_to_reproduce
 
     def update(self, is_in_sunlight: bool, is_raining: bool):
-        self.care_record["sunlight_exposure"] += is_in_sunlight
-        self.care_record["water_frequency"] += is_raining
+        self._care_record["sunlight_exposure"] += is_in_sunlight
+        self._care_record["water_frequency"] += is_raining
 
         # Day decay
-        self.care_record = {key: value - 1 for key, value in self.care_record.items()}
+        self._care_record = {key: value - 1 for key, value in self._care_record.items()}
 
-        self.grow()
+        self._grow()
         self.reproduction()
 
     def water(self):
-        self.care_record["water_frequency"] += 1
-
-    def status(self):
-        return f"{self.name} is a {self.__class__.__name__} at level {self.plant_level}."
+        self._care_record["water_frequency"] += 1
 
     def harvest(self):
         # You cant harvest a plant, but this methods will be used in Tree and Bush.
         pass
 
     def get_offspring(self):
-        self.reproduce_cooldown = 5
+        self._reproduce_cooldown = 5
 
         # cooping the plant
         offspring = copy.deepcopy(self)
         # reset attr
-        offspring.plant_level = 1
+        offspring._plant_level = 1
         offspring.ready_to_reproduce = False
-        offspring.reproduce_cooldown = 5
-        offspring.care_record = self.care_requirements
+        offspring._reproduce_cooldown = 5
+        offspring._care_record = self._care_requirements
 
         # Optionally, introduce variations
         # For example, slight changes in water_frequency or sunlight_exposure
@@ -80,6 +80,9 @@ class Plant:
 
         return offspring
 
+    def status(self):
+        return f"{self._name} is a {self.__class__.__name__} at level {self._plant_level}."
+
     def __repr__(self):
-        return (f"{self.emoji} {self.name}, l: {self.plant_level} w : {self.care_record['water_frequency']},"
-                f" s : {self.care_record['sunlight_exposure']}")
+        return (f"{self.emoji} {self._name}, l: {self._plant_level} w : {self._care_record['water_frequency']},"
+                f" s : {self._care_record['sunlight_exposure']}")
