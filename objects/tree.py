0eaca080ad186dfbb9db8001d34af235dd79ce81
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-06 16:20:25 +0100

    feat: private variables and new methods

--- objects/tree.py
+++ objects/tree.py
@@ -3,19 +3,43 @@ from .fruit import Fruit
 
 
 class Tree(Plant):
-    def __init__(self, name: str, emoji: str = '🌳'):
-        super().__init__(name, emoji)
-        self.blooming = False  # tree can bloom and later bears fruit.
-        self.bears_fruit = False
-        self.leaves = False
+    def __init__(self, name: str, emoji: str = '🌳', reproduce_level: int = 15):
+        super().__init__(name, emoji, reproduce_level)
+        self._blooming = False  # tree can bloom and later bears fruit.
+        self._bears_fruit = False
+        self._leaves = False
 
-    def foliate(self):
+    def _grow(self):
+        super()._grow()
+
+        # TODO: this is just a concept
+        self._blooming = self._plant_level >= 10
+        self._bears_fruit = self._plant_level >= 15
+        self._leaves = self._plant_level >= 5
+
+    def update(self, is_in_sunlight: bool, is_raining: bool):
+        super().update(is_in_sunlight, is_raining)
+
+        #  TODO: this is just a concept
+        if self._blooming:
+            self._bear_fruit()
+        if self._bears_fruit:
+            self._drop_leaves()
+        if self._leaves:
+            self._foliate()
+
+    def _foliate(self):
         pass
 
-    def drop_leaves(self):
+    # Based on time
+    def _drop_leaves(self):
         pass
 
-    def bear_fruit(self):
+    def harvest(self) -> Fruit:
+        # TODO: add functionality to harvest fruit
+        pass
+
+    def _bear_fruit(self):
         # Create fruit class
-        self.bears_fruit = Fruit()
+        self._bears_fruit = Fruit()
         pass
