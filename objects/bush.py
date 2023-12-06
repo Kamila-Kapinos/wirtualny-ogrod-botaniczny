b4f380878a0ecb26ac653c916335a04cef8a111b
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-06 16:17:00 +0100

    feat: renamed methods and higher reproduce_level

--- objects/bush.py
+++ objects/bush.py
@@ -3,18 +3,23 @@ from .fruit import Fruit
 
 
 class Bush(Plant):
-    def __init__(self, name: str, emoji: str = '😳'):
-        super().__init__(name, emoji)
-        self.bears_fruit = False
-        self.leaves = False
+    def __init__(self, name: str, emoji: str = '😳', reproduce_level: int = 10):
+        super().__init__(name, emoji, reproduce_level)
+        self._blooming = False  # tree can bloom and later bears fruit.
+        self._bears_fruit = False
+        self._leaves = False
 
-    def foliate(self):
+    def _foliate(self):
         pass
 
-    def drop_leaves(self):
+    def _drop_leaves(self):
         pass
 
-    def bear_fruit(self):
+    def harvest(self) -> Fruit:
+        # TODO: add functionality to harvest fruit
+        pass
+
+    def _bear_fruit(self):
         # Create fruit class
-        self.bears_fruit = Fruit()
+        self._bears_fruit = Fruit()
         pass
