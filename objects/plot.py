a61d50bf46d70505120b2b8dc20df38946736e73
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-06 16:20:00 +0100

    feat: private variables and new methods names, small reformating, __str__

--- objects/plot.py
+++ objects/plot.py
@@ -3,44 +3,50 @@ from .plant import Plant
 
 class Plot:
     def __init__(self):
-        self.contained_plant = None
+        self._contained_plant = None
 
     def place_plant(self, plant: Plant):
-        if self.contained_plant is not None:
+        if self._contained_plant is not None:
             raise ValueError("Plot is already occupied")
-        self.contained_plant = plant
+        self._contained_plant = plant
 
     def remove_plant(self):
-        if self.contained_plant is None:
+        if self._contained_plant is None:
             raise ValueError("Plot is empty")
 
         # method could return some information about the plant that was removed
-        self.contained_plant = None
+        self._contained_plant = None
 
-    def water(self):
-        if self.contained_plant is None:
+    def water_plant(self):
+        if self._contained_plant is None:
             raise ValueError("Plot is empty")
-        self.contained_plant.water()
+        self._contained_plant.water()
 
-    def harvest(self):
-        if self.contained_plant is None:
+    def harvest_plant(self):
+        if self._contained_plant is None:
             raise ValueError("Plot is empty")
-        self.contained_plant.harvest()
+        self._contained_plant.harvest()
 
-    def update(self):
-        pass
+    def update(self, is_in_sunlight: bool, is_raining: bool):
+        if self._contained_plant is None:
+            pass
+        self._contained_plant.update(is_in_sunlight, is_raining)
 
     def has_plant(self) -> bool:
-        return self.contained_plant is not None
+        return self._contained_plant is not None
 
     def is_ready_to_reproduce(self) -> bool:
-        if self.contained_plant is None:
+        if self._contained_plant is None:
             return False
-        return self.contained_plant.ready_to_reproduce
+        return self._contained_plant.ready_to_reproduce
 
     def get_plant_offspring(self) -> Plant:
-        if self.contained_plant is None:
+        if self._contained_plant is None:
             raise ValueError("Plot is empty")
-        return self.contained_plant.get_offspring()
+        return self._contained_plant.get_offspring()
+
     def __repr__(self):
-        return f"Plot({self.contained_plant})"
+        return f"Plot({self._contained_plant})"
+
+    def __str__(self):
+        return f"{self._contained_plant.emoji}"
