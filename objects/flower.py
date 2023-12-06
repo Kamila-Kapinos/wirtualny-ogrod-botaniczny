634fcd83c51b040eaf35968c7e4b50f9d3e13750
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-06 16:17:39 +0100

    feat: add random emoji and color while creating new Flower

--- objects/flower.py
+++ objects/flower.py
@@ -1,6 +1,10 @@
 from .plant import Plant
+from random import choice
 
 
 class Flower(Plant):
-    def __init__(self, name: str, emoji: str = '🌻'):
-        super().__init__(name, emoji)
+    def __init__(self, name):
+        super().__init__(name)
+        self.__colors = {"🌸": "white-pink", "🌼": "yellow", "🌻": "yellow", "🌺": "pomegranate-pink", "🌹": "red",
+                         "🌷": "pink", "🪻": "purple"}
+        self.emoji, self._color = choice(list(self.__colors.items()))
