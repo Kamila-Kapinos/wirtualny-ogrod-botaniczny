ff40992b24ec602ea3566d779e830afb42efec7d
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-06 16:18:13 +0100

    feat: add random emoji and color while creating new fruit and private variables

--- objects/fruit.py
+++ objects/fruit.py
@@ -1,5 +1,17 @@
+from random import choice
+
+
 class Fruit:
-    def __init__(self, name='test', color='test'):
-        self.name = name
-        self.is_edible = True
-        self.color = color
+    def __init__(self, name='test'):
+        self.__emojis_and_colors = {
+            "🍎": "red", "🍌": "yellow", "🍇": "purple",
+            "🍊": "orange", "🍓": "red", "🍉": "green-red", "🍍": "yellow-brown",
+            "🥜": "brown", "🌰": "dark-brown", "🥥": "white-brown"
+        }
+        self._name = name
+        self._is_edible = True
+        self._emoji, self._color = choice(list(self.__emojis_and_colors.items()))
+        self.status = "growing"  # growing / ready / rotten
+
+    def update(self):
+        pass
