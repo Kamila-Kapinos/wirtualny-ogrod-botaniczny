548243525a3b0958e641a0da574fb0a735e8e21b
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-29 15:16:39 +0100

    feat: change update and init for fruit, will behave as flower at the begging than will ripe into fruit

--- objects/fruit.py
+++ objects/fruit.py
@@ -3,15 +3,19 @@ from random import choice
 
 class Fruit:
     def __init__(self, name='test'):
+        self._color = None
+        self._emoji = '🌸'
+
         self.__emojis_and_colors = {
             "🍎": "red", "🍌": "yellow", "🍇": "purple",
             "🍊": "orange", "🍓": "red", "🍉": "green-red", "🍍": "yellow-brown",
             "🥜": "brown", "🌰": "dark-brown", "🥥": "white-brown"
         }
         self._name = name
-        self._is_edible = True
-        self._emoji, self._color = choice(list(self.__emojis_and_colors.items()))
-        self.status = "growing"  # growing / ready / rotten
-
+        self._is_ripe = False
+        
     def update(self):
-        pass
+        self._emoji, self._color = choice(list(self.__emojis_and_colors.items()))
+
+    def is_ripe(self):
+        return self._is_ripe
