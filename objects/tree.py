fdda9d455e554e3da8d0a77fe4c7c49ae2551ed7
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-29 15:16:56 +0100

    feat: add bloom harvest functions and logic

--- objects/tree.py
+++ objects/tree.py
@@ -3,18 +3,20 @@ from .fruit import Fruit
 
 
 class Tree(Plant):
-    def __init__(self, name: str, emoji: str = '🌳', reproduce_level: int = 15):
+    def __init__(self, name: str, emoji: str = '🌳', reproduce_level: int = 15, max_fruits: int = 15):
         super().__init__(name, emoji, reproduce_level)
         self._blooming = False  # tree can bloom and later bears fruit.
-        self._bears_fruit = False
+        self._fruit_bearing = False
         self._leaves = False
+        self._max_fruits = max_fruits
+        self._fruit_list = []
 
     def _grow(self):
         super()._grow()
 
         # TODO: this is just a concept
         self._blooming = self._plant_level >= 10
-        self._bears_fruit = self._plant_level >= 15
+        self._fruit_bearing = self._plant_level >= 15
         self._leaves = self._plant_level >= 5
 
     def update(self, is_in_sunlight: bool, is_raining: bool):
@@ -22,9 +24,11 @@ class Tree(Plant):
 
         #  TODO: this is just a concept
         if self._blooming:
-            self._bear_fruit()
-        if self._bears_fruit:
-            self._drop_leaves()
+            self._bloom()
+        if self._fruit_bearing:
+            # changes flower to fruit
+            pass
+        # Just droping leaves idk what for
         if self._leaves:
             self._foliate()
 
@@ -35,11 +39,19 @@ class Tree(Plant):
     def _drop_leaves(self):
         pass
 
-    def harvest(self) -> Fruit:
-        # TODO: add functionality to harvest fruit
-        pass
+    def harvest(self) -> list:
+        harvested = []
+        for index, fruit in enumerate(self._fruit_list):
+            if fruit.is_ripe():
+                harvested.append(self._fruit_list.pop(index))
+        return harvested
 
-    def _bear_fruit(self):
+    def _bloom(self) -> None:
+        # Create flower class
+        if len(self._fruit_list) < self._max_fruits:
+            self._fruit_list.append(Fruit())
+
+    def _fruitful(self) -> None:
         # Create fruit class
-        self._bears_fruit = Fruit()
-        pass
+        for fruit in self._fruit_list:
+            fruit.update()
